e141e7c1c618e629fc77e19d398e41bfeb0ba4fc
MikyGus <mikygus@gmail.com>
2024-08-10 17:53:10 +0200

    Fix Fade in and out

--- addons/menusystem/Transitions/FadeTransition.cs
+++ addons/menusystem/Transitions/FadeTransition.cs
@@ -20,54 +20,17 @@ public partial class FadeTransition : Transition
         transitionTo.Visible = false;
         transitionTo.GlobalPosition = Vector2.Zero;
 
-        await MenuController.Instance.CoverScreen.FadeToAlpha(1, TransitionTime);
-        transitionFrom.Visible = false;
-        transitionTo.Visible = true;
-        await MenuController.Instance.CoverScreen.FadeToAlpha(0, TransitionTime);
-
-        //await FadeOut(transitionFrom);
-        //await FadeIn(transitionTo);
+        if (FadeType.HasFlag(FadeType.Out))
+        {
+            await MenuController.Instance.CoverScreen.FadeToAlpha(1, TransitionTime);
+            transitionFrom.Visible = false;
+        }
+        if (FadeType.HasFlag(FadeType.In))
+        {
+            transitionTo.Visible = true;
+            await MenuController.Instance.CoverScreen.FadeToAlpha(0, TransitionTime);
+        }
     }
 
-    //public async Task FadeOut(Control transitionFrom)
-    //{
-    //    //Action actionOut = ActionFrom(transitionFrom, fadeTransitionType);
-    //    Action actionOut = null;
-    //    if (FadeType.HasFlag(FadeType.Out))
-    //    {
-    //        await FaderAsync(FadeType.Out, TransitionTime, actionOut);
-    //        transitionFrom.Visible = false;
-    //        //GD.Print($"FadeTransition-FadeOut: Wait is done");
-    //    }
-    //    else
-    //    {
-    //        actionOut?.Invoke();
-    //    }
-    //}
 
-    //public async Task FadeIn(Control transitionTo)
-    //{
-    //    //Action actionIn = ActionTo(transitionTo);
-    //    Action actionIn = null;
-    //    if (FadeType.HasFlag(FadeType.In))
-    //    {
-    //        transitionTo.Visible = true;
-    //        await FaderAsync(FadeType.In, TransitionTime, actionIn);
-    //        //GD.Print($"FadeTransition-FadeIn: FadeIn is done");
-    //    }
-    //    else
-    //    {
-    //        actionIn?.Invoke();
-    //    }
-    //}
-
-    //private async Task FaderAsync(FadeType fadeType, float transitionTime, Action action = null)
-    //{
-    //    Tween tween = CreateTween();
-    //    //GD.Print($"FadeTransition-Fade: {fadeType} tweening");
-    //    int finalVal = fadeType == FadeType.In ? 0 : 1;
-    //    tween.TweenProperty(_fadeScreen, Constants.PROPERTY_MODULATE_A, finalVal, transitionTime);
-    //    tween.TweenCallback(Callable.From(action));
-    //    await ToSignal(tween, Constants.EVENT_FINISHED);
-    //}
 }
